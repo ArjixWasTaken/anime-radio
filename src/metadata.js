01adf38eb2a8ebab5be34e0589b3d508aad496f5
Arjix <53124886+ArjixWasTaken@users.noreply.github.com>
2021-11-22 20:23:12 +0200

    Update metadata.js

--- src/metadata.js
+++ src/metadata.js
@@ -10,6 +10,14 @@ const updateTrackInfo = async () => {
     trackTitleNode.innerText = data.main.np;
     trackArtistNode.innerText = "DJ: " + data.main.djname;
     albumArtNode.src = djImageApi + data.main.dj.djimage;
+    
+    
+    const totalDuration = data.main.end_time - data.main.start_time
+    const remaining = data.main.end_time - (Date.now()/1000)
+    
+    const playedFor = totalDuration - remaining
+    document.querySelector("#songProgress").value = `${(playedFor/totalDuration) * 100}`
+
 };
 document.onload = updateTrackInfo;
 setInterval(updateTrackInfo, 5 * 1000);
